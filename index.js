1746206f3cdb7298f0622b13e2f8f4efc5901fcc
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-26 20:13:48 +0200

    feature : dependancy , error-handling , express static file

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const express = require("express");
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('public/index.html', { root: __dirname });
+  });
+
+  app.use('/staticfiles',express.static('public'));
+
+app.use((err,req,res, next) =>{
+console.error(err,stack);
+res.status(500).send('Something Broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
