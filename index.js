1e54a2b12bbbb521772ef7bd94b5da4cf369902d
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-27 19:03:50 +0200

    feat : mongoose connectivity , routes

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const express = require('express'),
-      morgan = require('morgan'),
-      bodyParser = require('body-parser');
-const app = express();
+const express = require("express"),
+      morgan = require("morgan"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      dotenv = require("dotenv")
+      dotenv.config(),
+      userRoute = require("./routes/user"),
+      authRoute = require("./routes/auth")
+      const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('public/index.html', { root: __dirname });
-  });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Database Connection Successful"))
+.catch(err => console.log(err));
+  
 
-  app.use('/staticfiles',express.static('public'));
+app.get('/', (req,res) => {
+  res.send("welcome to Zooplus Ecommerce API")
+})
 
+//gets all the users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((user) => {
+      res.status(201).json(user);
+      console.log("fetch success")
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.use(express.json())
+app.use("/api/user",userRoute)
+app.use("/api/auth",authRoute)
+
+
+//Error handling Middleware
 app.use((err,req,res, next) =>{
 console.error(err,stack);
 res.status(500).send('Something Broke!');
 });
 
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+app.listen(process.env.PORT || 3000,() => {
+  console.log('Backend server is running');
+});
