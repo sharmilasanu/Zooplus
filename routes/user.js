03f6d64f2c42a2a21357cc6fc60ef2675d7d2483
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-28 21:01:06 +0200

    add : Endpoint for User model

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,35 @@
+const User = require("../models/user");
 const router = require("express").Router()
 
-router.get("/",(req,res)=> {
-    res.send("user test is successful")
-})
+
+//search all users
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  });
+//search user by id
+  router.get("/search/:id", async (req, res) => {
+    try {
+      const user = await User.findOne({id:req.params.id});
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  //User delete
+  router.delete("/:id", async (req, res) => {
+    try {
+      const user = await User.findOneAndDelete({id:req.params.id});
+      res.status(200).json("user been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 router.post("/",(req,res) => {
     const username = req.body.username;
