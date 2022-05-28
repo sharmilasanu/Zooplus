ae833d09cff5afa1c6b817234f6b1dad9c891bd5
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-28 18:19:05 +0200

    add: routes for Product, users

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express"),
       morgan = require("morgan"),
-      bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
-      dotenv = require("dotenv")
-      dotenv.config(),
       userRoute = require("./routes/user"),
-      authRoute = require("./routes/auth")
+      authRoute = require("./routes/auth"),
+      productRoute = require("./routes/products")
       const app = express();
-
+const dotenv = require("dotenv");
+dotenv.config()
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("Database Connection Successful"))
 .catch(err => console.log(err));
@@ -17,22 +16,11 @@ app.get('/', (req,res) => {
   res.send("welcome to Zooplus Ecommerce API")
 })
 
-//gets all the users
-app.get('/users', (req, res) => {
-  Users.find()
-    .then((user) => {
-      res.status(201).json(user);
-      console.log("fetch success")
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
 app.use(express.json())
-app.use("/api/user",userRoute)
-app.use("/api/auth",authRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/products", productRoute)
+app.use("/api/users", userRoute)
+
 
 
 //Error handling Middleware
