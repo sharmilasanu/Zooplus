8c3221d5e97beba94f2dd5e56d463127751e28a6
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-05-29 22:24:48 +0200

    update : changed mongoose query

--- routes/user.js
+++ routes/user.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
 //search user by id
   router.get("/search/:id", async (req, res) => {
     try {
-      const user = await User.findOne({id:req.params.id});
+      const user = await User.findById(req.params.id);
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json(err);
